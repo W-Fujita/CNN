dd1a2877b2a5e93c6de382f41499f9d02f1cdfa4
wataru <wataru.101001@docomo.ne.jp>
2020-02-08 14:32:12 +0900

    Add progress bar

--- scrape.py
+++ scrape.py
@@ -12,8 +12,8 @@ class Google:
         self.session = requests.session()
         self.session.headers.update({"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0"})
 
-    #Google検索
     def search(self, keyword, type, maximum):
+        """Google検索"""
         print("Google {} Search: {}".format(type.capitalize(), keyword))
         result = []
         total = 0
@@ -32,12 +32,11 @@ class Google:
             else:
                 result += links
                 total += len(links)
-                
         print("-> 結果{}のlinksを取得しました".format(str(len(result))))
         return result
 
-    #検索クエリジェネレータ
     def query_gen(self, keyword, type):
+        """検索クエリジェネレータ"""
         page = 0
         while True:
             if type == "text":
@@ -55,8 +54,8 @@ class Google:
             yield self.SEARCH_URL + "?" + params
             page += 1
 
-    #リンク取得
     def get_links(self, html, type):
+        """リンク取得"""
         soup = BeautifulSoup(html, "lxml")
         if type == "text":
             elements = soup.select(".rc > .r > a")
@@ -71,22 +70,19 @@ class Google:
 def main():
     import os
     os.makedirs("./Original", exist_ok=True) #スクレイピングで取得した画像用フォルダ
-    os.makedirs("./Edited", exist_ok=True) #サイズを変更した画像用フォルダ
-    os.makedirs("./Train", exist_ok=True) #トレーニングデータの画像用フォルダ
-    os.makedirs("./Test", exist_ok=True) #テストデータの画像用フォルダ
-    
+    bar_template = "\r{0}% [{1}]"
+
     #画像のURLをgoogle検索から取得する
     google = Google()
     for word in search_word:
         img_URLs = google.search(word, "image", limit) #画像検索
         os.makedirs("./Original/"+str(word), exist_ok=True) #保存先のフォルダの作成
-
         #Originalファイルに画像を保存する
         for i,target in enumerate(img_URLs): #img_URLsからtargetに入れる
             try:
                 re = requests.get(target, allow_redirects=False)
-                with open("./Original/"+str(word)+"/"+str(i)+".jpg", "wb") as f: #imgフォルダに格納
-                    f.write(re.content) #.contentにて画像データとして書き込む
+                with open("./Original/"+str(word)+"/"+str(i)+".jpg", "wb") as f: #Originalフォルダに格納
+                    f.write(re.content) #.contentで画像データとして書き込む
             except requests.exceptions.ConnectionError:
                 continue
             except UnicodeEncodeError:
@@ -95,8 +91,12 @@ def main():
                 continue
             except IsADirectoryError:
                 continue
+            #進歩バーの表示
+            num = int((i+1)/len(img_URLs)*20)
+            bar = "#"*num + " "*(20-num)
+            print(bar_template.format(num*5, bar), end="")
+        print()
+    print("\n保存完了しました")
 
-    print("保存完了しました")
-    
 if __name__ == "__main__":
     main()
